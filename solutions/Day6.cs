52da4cfb8936823c4ca1adfa8a018732b8d8c0a7
Justin Morris <JMorris@parata.com>
2023-01-13 12:13:29 -0500

    switched pt 2 to a window back propagation first seen by David Perez

--- solutions/Day6.cs
+++ solutions/Day6.cs
@@ -7,21 +7,41 @@ internal readonly struct Day6 : IAdventDay<int>
     int IAdventDay<int>.Part1() => Windowed(data, 4)
         .First(static set => !set.Item3).Item1 + 4;
 
-    int IAdventDay<int>.Part2() => Windowed(data, 14)
-        .First(static set => !set.Item3).Item1 + 14;
+    int IAdventDay<int>.Part2() => BackWindowProp(data, 14);
 
-    private static IEnumerable<(int,string,bool)> Windowed(string source, int size)
+    private static int BackWindowProp(in string data, in int windowSize)
+    {
+        int l = data.Length, index = 0;
+        while (index < l)
+        {
+            int state = 0, pos = windowSize - 1;
+            while (pos >= 0)
+            {
+                int bitIndex = 1 << (data[index + pos--] % 32);
+                if ((state & bitIndex) is not 0) break;
+                state |= bitIndex;
+            }
+            if (pos is -1) return index + windowSize + 1;
+            index += pos + 1;
+        }
+        return -1;
+    }
+
+    //int IAdventDay<int>.Part2() => Windowed(data, 14)
+    //    .First(static set => !set.Item3).Item1 + 14;
+
+    private static IEnumerable<(int, string, bool)> Windowed(string source, int size)
     {
         int l = source.Length, i = -1;
         while (++i < l)
-        { 
+        {
             if (i + size > l) yield break;
 
             string buffer = source.Substring(i, size);
-            
+
             HashSet<char> uniqueMap = new(buffer);
 
-            yield return 
+            yield return
             (
                 i, // Index
                 buffer, // Elements
